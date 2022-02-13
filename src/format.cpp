6486e001ecad1b0f1f1bacb62f66d7e9d484a5b3
Yoko Harada <yokolet@gmail.com>
2022-02-13 04:55:07 +0000

    adds format impl

--- src/format.cpp
+++ src/format.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <string>
 
 #include "format.h"
@@ -8,4 +9,12 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  long h = seconds / 3600;
+  int m = (seconds % 3600) / 60;
+  int s = seconds % 60;
+  char buffer[std::max<int>((std::to_string(h)).length(), 2) + 4];
+  sprintf(buffer, "%02ld:%02d:%02d", h, m, s);
+  string result(buffer);
+  return result;
+}
